96b74c61e62be1e3b2089d8d292f7f87c767e887
ErickDKnight <erickmontesdk@gmail.com>
2025-11-22 18:51:20 -0800

    editing deply.ml

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Servidor escuchandos en http://localhost:${port}`);
+    console.log(`Servidor escuchando en http://localhost:${port}`);
 });
